2063df38278dfec38a572c515db840bafd8216be
gptus <yhc9308@naver.com>
2023-08-16 12:52:53 +0900

    리엑트 컴포넌트 기본 사용법

---
+++ Chap4/try-react/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import AnotherScreen from "./AnotherScreen";
+import { Switch, Route } from "react-router";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Switch>
+          <Route exact={true} path="/" component={Home}></Route>
+          <Route path="/another" component={AnotherScreen}></Route>
+        </Switch>
+      </header>
+    </div>
+  );
+}
+
+export default App;
