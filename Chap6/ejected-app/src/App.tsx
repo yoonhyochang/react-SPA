6b62b3f142e71b1e2a8ec4c3577ba9e941a1e96e
gptus <yhc9308@naver.com>
2023-08-16 20:16:26 +0900

    create-react-app을 사용한 프로젝트 설정과 jest 활용 테스트

---
+++ Chap6/ejected-app/src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import DisplayText from "./DisplayText";
+import "./App.css";
+
+function App() {
+  const getUserFullname = async (username: string): Promise<string> => {
+    const usersResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    if (usersResponse.ok) {
+      const users = await usersResponse.json();
+      const userByName = users.find((usr: any) => {
+        return usr.username.toLowerCase() === username;
+      });
+      return userByName.name;
+    }
+    return "";
+  };
+  return (
+    <div className="App">
+      <DisplayText getUserFullname={getUserFullname} />
+    </div>
+  );
+}
+
+export default App;
