298ddb253d0755654da70f9bbdbf3bbe5905cfef
gptus <yhc9308@naver.com>
2023-08-17 14:01:51 +0900

    Chap7

---
+++ Chap7/redux-sample/src/UserNameComp.tsx
@@ -0,0 +1,14 @@
+import React, { useContext, useRef } from "react";
+import { TestContext } from "./ContextTester";
+
+const UserNameComp = React.memo(() => {
+  const renders = useRef(0);
+  console.log("renders UserNameComp", renders.current++);
+
+  const { username } = useContext(TestContext);
+  console.log("username UserNameComp", username);
+
+  return <div>{username}</div>;
+});
+
+export default UserNameComp;
---
+++ Chap7/redux-sample/src/store/configureStore.ts
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+import { rootReducer } from "./AppState";
+
+const configureStore = () => {
+  return createStore(rootReducer, {});
+};
+export default configureStore;
